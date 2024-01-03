0a24cf931a583dcaadd5b5b00a31d85cf1baae59
haniyeh <heydarihaniyeh51@gmail.com>
2024-01-03 21:34:07 +0330

    add error name

--- login-register.js
+++ login-register.js
@@ -10,13 +10,10 @@ signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
 
-function check() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if (email === "" || password === "") {
-        document.getElementById('error').textContent = "همه فیلد ها را پر کنید";
-    } else {
-        document.getElementById('error').textContent = "Incorrect username or password";
-    }
 
+function validateForm() {
+    var name = document.getElementById("name").value;
+    if (name === "") {
+        document.getElementById("name").classList.add("error");
+    } else {}
 }
